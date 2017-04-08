5d8a5d6f521363d14095a07fb78400705b742ee8
Kang Yun Wang <kevinwang8011@hotmail.com>
2017-04-08 15:15:50 +0200

    Matching
    The displayed users in the meet page for users are now matched
    displayed according to the preferred age and gender

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/meet', (req, res) => {
+router.get('/meet', (req, res, next) => {
+  let userSelf;
   req.getConnection((err, connection) => {
     if(err) return next(err);
+    connection.query('SELECT * FROM user WHERE userID = ?', myID, (err, results) => {
+      if (err) return next(err);
+      userSelf = results[0];
 
-    connection.query('SELECT userID, name,TIMESTAMPDIFF(YEAR, dob, CURDATE()) AS age, location FROM user WHERE admin = 0 AND userID != ?', myID, (err, results) => {
-      if(err) return next(err);
-
-      res.locals.results = results;
-      res.render('user/meet');
+      // new query to be able to utilise userSelf
+      connection.query('SELECT userID, name, TIMESTAMPDIFF(YEAR, dob, CURDATE()) AS age, location FROM user WHERE admin = 0 AND userID != ? AND IF (? = "both", gender = "male" OR gender = "female", gender = ?) HAVING age BETWEEN ? AND ?', [myID, userSelf.lookingFor, userSelf.lookingFor, userSelf.minAge, userSelf.maxAge], (err, results) => {
+        if(err) return next(err);
+        res.locals.results = results;
+        res.render('user/meet');
+      });
     });
   });
 });
 
-router.get('/meet_random', (req, res) => {
+router.get('/meet_random', (req, res, next) => {
+  let userSelf;
   req.getConnection((err, connection) => {
-    if (err) return next(err);
-    connection.query('SELECT userID FROM user WHERE userID != ?', myID, (err, results) => {
+    if(err) return next(err);
+    connection.query('SELECT * FROM user WHERE userID = ?', myID, (err, results) => {
       if (err) return next(err);
-      const i = Math.floor(Math.random() * results.length);
+      userSelf = results[0];
 
-      res.render('user/meet_random', {results: results[i].userID});
-    });
+      connection.query('SELECT userID, name, TIMESTAMPDIFF(YEAR, dob, CURDATE()) AS age, location FROM user WHERE admin = 0 AND userID != ? AND IF (? = "both", gender = "male" OR gender = "female", gender = ?) HAVING age BETWEEN ? AND ?', [myID, userSelf.lookingFor, userSelf.lookingFor, userSelf.minAge, userSelf.maxAge], (err, results) => {
+        if(err) return next(err);
+        const i = Math.floor(Math.random() * results.length); // randomly select a match
+        res.render('user/meet_random', {results: results[i].userID});
+      });
+  });
+
+
   });
 });
 
